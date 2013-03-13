8187c37141cc1e4dba784b77760ced5d8e618ea2
Michael Kamphausen <michael.kamphausen@apploft.de>
2013-03-13 16:31:20 +0100

    Changed attribute types from "boolean" to "string", because according to http://wiki.opscode.com/display/chef/Metadata#Metadata-attribute only "string" and "array" are allowed.

--- dependencies/recipes/update.rb
+++ dependencies/recipes/update.rb
@@ -8,26 +8,26 @@ include_recipe 'gem_support'
 case node[:platform]
 when 'centos','redhat','fedora','amazon'
   #if node[:dependencies][:upgrade_rpms] - not implemented in the application jet
-  if node[:dependencies][:upgrade_debs]
+  if node[:dependencies][:upgrade_debs] == 'true'
     execute 'yum -y update' do
       action :run
     end
   end
 when 'debian','ubuntu'
-  if node[:dependencies][:update_debs]
+  if node[:dependencies][:update_debs] == 'true'
     execute 'apt-get update' do
       action :run
     end
   end
 
-  if node[:dependencies][:upgrade_debs]
+  if node[:dependencies][:upgrade_debs] == 'true'
     execute 'apt-get upgrade -y' do
       action :run
     end
   end
 end
 
-if node[:dependencies][:upgrade_gems]
+if node[:dependencies][:upgrade_gems] == 'true'
   execute 'gem update' do
     action :run
   end
